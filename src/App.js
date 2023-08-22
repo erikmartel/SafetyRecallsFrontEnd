c7d863a349db32565a7ee3c585ea598ae63658b6
erikmartel <erikdmartel@gmail.com>
2023-08-22 14:21:13 -0400

    Initial front-end HTML and CSS

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-    <h1>Safety Recall Checker</h1>
+     
+
+      <div className="appContainer">
+      <header className="appHeader">
+        <h1 className="appTitle">Safety Recall Checker</h1>
       </header>
+        <div className= "checkerContainer">
+          
+            <label className="driverSelectorLabel" for="driverSelector">Who's driving?</label>
+            <select className='driverSelectorDropdown' id="driverSelector">Who's driving?</select>
+            <button color = "#1AFFFF" className="recallSubmitButton">Check my recalls</button>
+          
+        </div>
+      </div>
     </div>
   );
 }
