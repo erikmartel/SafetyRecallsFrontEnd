5fdf853ac9a1e545ce8a2416c05dc36cabb8a6c7
Dayna Tynan <Dayna_Regan@yahoo.com>
2023-08-22 15:44:35 -0400

    working on presenting data in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Header from './components/header/header';
 import Checker from './components/checker/checker';
-
+import { useState, useEffect } from "react";
 import RecallCard from './components/recallCard/recallCard';
+import React from 'react';
 
 
 function App() {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const url = `https://api.nhtsa.gov/recalls/recallsByVehicle?make=jeep&model=wrangler&modelYear=2018`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setError(false)
+        setData(data)
+      }).catch(error => setError(true))
+  }, [])
+
+  console.log(data)
+
   return (
     <div className="App">
       <Header />
@@ -16,10 +32,23 @@ function App() {
 
       <div className="recallListContainer">
       <RecallCard />
-        
       </div>
+      <div className="recallData">
+      {/* {
+        data
+          ? (
+            <div>{data}</div>
+          )
+          : <span>Loading...</span>
+      }
+      {
+        error
+          ? <span>There was a problem loading this data.</span>
+          : null
+      } */}
     </div>
-  );
+    </div>
+    );
 }
 
 export default App;
