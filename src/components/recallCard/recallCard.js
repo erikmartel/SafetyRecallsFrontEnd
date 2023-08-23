4d82bfd004aef8825a2286635bb85b33a53544b2
erikmartel <erikdmartel@gmail.com>
2023-08-23 11:32:53 -0400

    data mapped into card components

--- src/components/recallCard/recallCard.js
+++ src/components/recallCard/recallCard.js
@@ -5,27 +5,25 @@ import { CCard, CCardBody, CCardLink, CCardSubtitle, CCardText, CCardTitle } fro
 function RecallCard ({data}) {
   console.log(data)
     return(
-        <><CCard className="recallCard" style={{ width: '18rem' }}>
-        <CCardBody>
-          <CCardTitle className="cardTitle">Card title</CCardTitle>
-          <CCardSubtitle className="mb-2 text-medium-emphasis">Card subtitle</CCardSubtitle>
-          <CCardText>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </CCardText>
-          <CCardLink className="cardLink" href="#">Card link</CCardLink>
-        </CCardBody>
-      </CCard>
+        <>
       <><div>
       {
          data.map(data=>{
           return(
-            <div key={data.NHTSACampaignNumber} style={{alignItems:'center',margin:'20px 60px'}}>
-            <h4>{data.ModelYear} {data.Make} {data.Model}</h4>
-            <p><b>Component:</b> {data.Component}</p>
-            <p><b>Summary:</b> {data.Summary}</p>
+            <div>
+             <CCard key={data.NHTSACampaignNumber} className="recallCard" style={{ width: 'auto' }}>
+              <CCardBody>
+                  <CCardTitle className="cardTitle">{data.ModelYear} {data.Make} {data.Model}</CCardTitle> 
+                    <CCardSubtitle className="mb-2 text-medium-emphasis">{data.Component}</CCardSubtitle>
+                      <CCardText>
+                      {data.Summary}
+                      </CCardText>
+                    <CCardLink className="cardLink" href="#">Card link</CCardLink>
+              
+              </CCardBody>
+              </CCard>
           </div>
           )
-
         })
       }
         </div></></>
